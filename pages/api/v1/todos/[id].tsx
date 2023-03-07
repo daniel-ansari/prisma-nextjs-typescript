abd11e659bb1133f0a946afb036e16e5395facde
Daniel Ansari <d0nsari2020@gmail.com>
2023-03-07 14:35:49 +0700

    add next cors

--- pages/api/v1/todos/[id].tsx
+++ pages/api/v1/todos/[id].tsx
@@ -1,10 +1,19 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
 import { Todo } from "../../../../models/Todo"
+import NextCors from 'nextjs-cors';
+
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
   const id = String(req.query.id);
   if (req.method === 'GET') {
     const todo = await Todo.find(id)
